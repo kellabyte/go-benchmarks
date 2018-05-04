9874870c1e57c0667de3e93f6cd9e26180849369
Kelly Sommers <kell.sommers@gmail.com>
2018-05-04 01:41:22 -0400

    Adding initial R plotting files.

---
+++ plotting/gobench.r
@@ -0,0 +1,34 @@
+# Rscript gobench.r gobench.out output.png
+
+#Parse the command
+args = commandArgs(trailingOnly=TRUE) # this line only works when you run this script from the command line.
+
+library(tidyverse)
+library(drlib)
+
+plot = readr::read_delim(args[1], "\t", skip = 5, col_names = FALSE) %>%
+  set_names("Name", "Ops", "NsPerOp", "MBPerS", "AllocatedBytesPerOp", "AllocationsPerOp") %>%
+
+  #Clean up the text so we get numbers...
+  mutate(NsPerOp = as.numeric(str_replace(NsPerOp, " ns/op", "")),
+         MBPerS = as.numeric(str_replace(MBPerS, " MB/s", "")),
+         AllocatedBytesPerOp = as.numeric(str_replace(AllocatedBytesPerOp, " B/op", "")),
+         AllocationsPerOp = as.numeric(str_replace(AllocationsPerOp, " allocs/op", "")),
+         Ops = as.numeric(Ops)) %>%
+  filter(!is.na(NsPerOp)) %>%
+
+  # Split out the cores.
+  separate(Name, c("Name", "Cores"), "-") %>%
+  mutate(Cores = as.numeric(Cores)) %>%
+  
+  # Split out the bytes.
+  separate(Name, c("Name", "Bytes"), "/") %>%
+  mutate(Bytes = as.numeric(Bytes)) %>%
+  
+  # Generate the plots.
+  ggplot(aes(Name, NsPerOp)) + 
+  geom_col() + 
+  rotate_x_labels(vjust = .5) +
+  facet_wrap(~ Bytes, scales="free_y")
+
+ggsave(args[2], plot, width = 16, height = 9)
--- plotting/setup.r
+++ plotting/setup.r
@@ -1,2 +1,6 @@
-install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
-install.packages("ggplot2")
+#install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+install.packages("devtools", repos = "http://cran.us.r-project.org")
+library(devtools)
+devtools::install_github("dgrtwo/drlib")
